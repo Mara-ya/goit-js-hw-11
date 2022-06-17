78743aac6797684e8534883f900a782e9c84d063
Mara-ya <mariya18.99.08@gmail.com>
2022-06-17 23:02:52 +0300

    message

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,9 @@ import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = "feedback-form-state";
-const formData = {};
+
+const parseStorageKey = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const formData = {...parseStorageKey};
 
 form.addEventListener('submit', onFromSubmit);
 form.addEventListener('input', throttle(onInput, 500));
@@ -11,20 +13,18 @@ completedForm();
 
 function onFromSubmit(evt) {
     evt.preventDefault();
+    console.log(formData);
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    formData.email = '';
+    formData.message = '';
 }
 
 function onInput(evt) {
     formData[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 function completedForm() {
-    const localData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if(localData){
-        console.log(localData);
-        form.elements.email.value = localData.email;
-        form.elements.message.value = localData.message;
-    }
+    formData.email === undefined ? form.elements.email.value = "" : form.elements.email.value = formData.email;
+    formData.message === undefined ? form.elements.message.value = "" : form.elements.message.value = formData.message;
 }
