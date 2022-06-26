0bac355d57a0814e7491c5c164de93bccff8cd93
Mara-ya <mariya18.99.08@gmail.com>
2022-06-26 22:22:42 +0300

    message

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formData = document.querySelector('.form');
+
+formData.addEventListener('submit', onSubmit)
+
+function onSubmit(e){
+  e.preventDefault();
+  const {
+  elements: { delay, step, amount }
+  } = e.currentTarget;
+  let delayTime = Number(delay.value);
+  const stepTime = Number(step.value);
+  const amountTime = Number(amount.value);
+
+  for (let position = 1; position <= amountTime; position++){
+    createPromise(position, delayTime)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    delayTime += stepTime;
+  };
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve,reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() =>{
+      if (shouldResolve) {
+        resolve({position, delay});
+      }
+      reject({position, delay});
+    }, delay);
+  });
 }
